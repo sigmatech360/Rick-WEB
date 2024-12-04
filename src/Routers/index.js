b5dd3553f33f40020322974986ef4fefaf4f0072
sigmatech360 <syed.danial@sigmatechservices.net>
2024-12-03 16:07:13 -0800

    add

--- src/Routers/index.js
+++ src/Routers/index.js
@@ -14,7 +14,7 @@ import Ourpodcast from "../Screens/ourpodcast";
 
 export default function UserRouter() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/His-oc">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About/>} />
